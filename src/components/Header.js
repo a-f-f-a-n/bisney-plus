170e64659c94dc21afdf3addd412de854e6d9a87
Affan Gazi <affangazi0@gmail.com>
2022-09-19 01:13:00 -0400

    ONLY CHANGED HEADER A LOT

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,28 @@ function Header() {
                     <span>HOME</span>
 
                 </a>
+                <a>
+                    <img src = '/images/search-icon.svg' />
+                    <span>SEARCH</span>
+
+                </a>
+                <a>
+                    <img src = '/images/watchlist-icon.svg' />
+                    <span>WATCHLIST</span>
+
+                </a>
+                <a>
+                    <img src = '/images/original-icon.svg' />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src = '/images/movie-icon.svg' />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src = '/images/series-icon.svg' />
+                    <span>SERIES</span>
+                </a>
            </Nav_Menu>
         </Nav>
 
@@ -30,22 +52,56 @@ const Nav = styled.nav`
     padding: 0 36px;
     `
 const Nav_Menu = styled.div`
-    display: flex
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
     a {
         display: flex;
         align-items: center;
-        padding: 0 12px;
+        padding: 0 20px;
+        cursor: pointer;
         img {
             height: 20px;
         }
         span {
             font-size: 13px;
-            letter-spacing: 
+            font-weight: bold;
+            letter-spacing: 1.5px;
+            position: relative;
+            padding: 0 10px;
+            &:after { 
+                content: '';
+
+                height: 2px;
+                position: absolute;
+                
+                left: 0;
+                right: 0;
+                bottom: -6px;
+
+                background: white;
+                opacity: 0;
+
+                transform-origin: left center;
+                transform: scaleX(0);
+                
+                transition: all 250ms cubic-bezier(0.25, 0.45, 0.45; 0.95) 0s; 
+            }
+        }
+        &:hover {
+            span:after {
+                opacity: 1;
+                transform: scaleX(1);
+            }
         }
     }`
-const Logo = styled.img`
-    width: 80px;
-`
 
+// the & is the reference to the parent selector just to make it look nicer
+//:after is a pseudo element while :hover is a pseudo class
+const Logo = styled.img`
+    width: 75px;
+`
+//NEVER FORGET THE SEMI COLON IN THE CSS AKA STYLED
 //styled gives js a div. It is a reference to <div>
 
