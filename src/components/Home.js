1ca84762bda22f283d977bb8a859a56412e059da
Affan Gazi <affangazi0@gmail.com>
2022-09-22 01:06:35 -0400

    messed up the image slider for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react' 
 import styled from 'styled-components'
-import styled from 'styled-components'
 import ImageSlide from './ImageSlide'
 function Home() {
     return (
--- src/components/ImageSlide.js
+++ src/components/ImageSlide.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css' //i need these for the long slider thingys on disney plus
+//import 'slick-carousel/slick/slick.css'
+import styled from 'styled-components'
+//import 'slick-carousel/slick/slick-theme.css' //i need these for the long slider thingys on disney plus
 
 function ImageSlide() {
     let settings = {
@@ -13,11 +14,42 @@ function ImageSlide() {
         autoplay: true
     }
     return (
-        <Slides>
-            Slider
+        <Slides {...settings}>
+            <Wrap>
+                <img src = '/images/nationalgeo1440.jpg' />
+            </Wrap>
+                <img src = '/images/thor1440.jpg' />
         </Slides>
     )
 }
 
 export default ImageSlide
 
+const Slides = styled(Slider)`
+    ul li button {
+        &:before {
+           font-size 10px;
+       }
+    }
+    li.slick-active button:: before {
+        color: white;
+    }
+    
+    .slick-list {
+        overflow:visible
+    }
+    margin-top: 20px
+
+    button {
+        z-index: 1; //without this there would be no previous button
+    }
+`
+const Wrap = styled.div`
+    img {
+        border: 4px solid transparent;
+        border-radius: 4px; //allows rounded corners
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 70%) 0px 25px 30px -10px, rgb( 0 0 0 / 75%) 0px 15px 10px -10px;
+    }
+`
